558b553247816e2dfcf99baabc594846c971574c
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-18 16:03:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('change-image').addEventListener('click',image)
+document.getElementById('change-text').addEventListener('click',text)
+document.getElementById('hide').addEventListener
+('click', hide)
+document.getElementById('show').addEventListener
+('click', show)
+document.getElementById('background').addEventListener
+('click', background)
+
+function image () {
+  document.getElementById('picture').src="./Karelian-Bear-Dog.jpg"
+  }
